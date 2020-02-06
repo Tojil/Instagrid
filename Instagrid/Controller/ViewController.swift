93eb23904162c031c8c0b6f21ae883af00c7c033
Sergio Canto <sergio@bellcantolai.com>
2020-02-07 00:54:21 +0100

    Projet 4 le debut

---
+++ Instagrid/Controller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Sergio Canto  on 28/01/2020.
+//  Copyright © 2020 Sergio Canto . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of an resources that can be recreated
+    }
+}
+
