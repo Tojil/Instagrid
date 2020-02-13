9a3083732a4785292786d35f559de0091c6f14cd
Sergio Canto <sergio@bellcantolai.com>
2020-02-13 17:06:56 +0100

    l'inteface est finis git push git status

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var layoutButtons: [UIButton]!
+    @IBOutlet weak var topRightGridButton: UIButton!
+    @IBOutlet weak var bottomRightGridButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of an resources that can be recreated
+    @IBAction func layoutButtonTapped(_ sender: UIButton) {
+        layoutButtons.forEach { $0.isSelected = false }
+        sender.isSelected = true
+        switch sender.tag {
+        case 0:
+            topRightGridButton.isHidden = true
+            bottomRightGridButton.isHidden = false
+        case 1:
+            topRightGridButton.isHidden = false
+            bottomRightGridButton.isHidden = true
+        case 2:
+            topRightGridButton.isHidden = false
+            bottomRightGridButton.isHidden = false
+        default:
+            break
+        }
     }
+    
 }
 
