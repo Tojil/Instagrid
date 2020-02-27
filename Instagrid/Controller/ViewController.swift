8ef9d05a8773ff29c066d048969699437f9afea9
Sergio Canto <sergio@bellcantolai.com>
2020-02-27 09:57:37 +0100

    Orientation controlé

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -19,49 +19,73 @@ class ViewController: UIViewController {
     
     let imagePickerController = UIImagePickerController()
     var tag = 0
-    var screenWidth = UIScreen.main.bounds.height
-    let swipeUpGesture = UISwipeGestureRecognizer(target: self, action: #selector(dragGridCentral(swipe:)))
-    
-    
-    
-   
+    var screenWidth = UIScreen.main.bounds.width
+    var screenHeight = UIScreen.main.bounds.height
+    var swipeGesture: UISwipeGestureRecognizer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePickerController.delegate = self
-        let swipeUpGesture = UISwipeGestureRecognizer(target: self, action: #selector(dragGridCentral(swipe:)))
-        swipeUpGesture.direction = .up
-        view.addGestureRecognizer(swipeUpGesture)
+        swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(dragGridCentral(swipe:)))
         
-
+        guard let swipeGesture = swipeGesture else{return}
+        //swipeGesture.direction = .up
+        gridCentral.addGestureRecognizer(swipeGesture)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleSwipeDirection), name: UIDevice.orientationDidChangeNotification, object: nil)
     }
-    
+
     
     @objc private func dragGridCentral(swipe: UISwipeGestureRecognizer) {
-        UIView.animate(withDuration: 0.5, animations: {
-            self.gridCentral.transform = CGAffineTransform(translationX: 0, y: -self.screenWidth)
-        }) { _ in
-            print("l'animation est términée")
-            UIView.animate(withDuration: 2) {
-               self.gridCentral.transform = .identity
-            }
+        if swipe.direction == .up {
+            print("je swipe vers le haut")
+        }else {
+            print("je swipe vers la gauche")
         }
-       
     }
     
-    func handleSwipeDirection() {
-        let myDevice = UIDevice.current.orientation
-        if myDevice.isPortrait {
-            swipeUpGesture.direction = .up
-            print("Portrait")
-        } else if myDevice.isLandscape{
-            swipeUpGesture.direction = .left
-            print("LandScape")
+    // si le telephone est en portrait on change la direction du swipe pour le mettre en point up haut
+    // si non on met le drirection du swipe sur point left
+    @objc func handleSwipeDirection() {
+        if UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight {
+            print("left")
+            swipeGesture?.direction = .left
+        }else {
+            print("up")
+            swipeGesture?.direction = .up
+           
         }
-        // si le telephone est en portrait on change la direction du swipe pour le mettre en point up haut
-        // si non on met le drirection du swipe sur point left
+//        let myDevice = UIDevice.current.orientation
+//        if myDevice.isPortrait {
+//            swipeGesture?.direction = .up
+//            UIView.animate(withDuration: 0.5, animations: {
+//                self.gridCentral.transform = CGAffineTransform(translationX: 0, y: -self.screenHeight)
+//            }) { _ in
+//                UIView.animate(withDuration: 2) {
+//                   self.gridCentral.transform = .identity
+//                }
+//            }
+//            //shareSwipeView()
+//            print("Portrait")
+//
+//        } else if myDevice.isLandscape{
+//            swipeGesture?.direction = .left
+//            UIView.animate(withDuration: 0.5, animations: {
+//                self.gridCentral.transform = CGAffineTransform(translationX: -self.screenWidth, y: 0)
+//            }) { _ in
+//                UIView.animate(withDuration: 2) {
+//                   self.gridCentral.transform = .identity
+//                }
+//            }
+//            //shareSwipeView()
+//            print("LandScape")
+//        }
     }
     
+//    func shareSwipeView() {
+//        let activityVC = UIActivityViewController(activityItems: [gridCentral ?? UIView.self], applicationActivities: nil)
+//        activityVC.popoverPresentationController?.sourceView = self.view
+//        self.present(activityVC, animated: true, completion: nil)
+//    }
     
     
     @IBAction func addPhoto(_ sender: UIButton) {
